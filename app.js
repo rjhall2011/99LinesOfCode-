aa3f79ed075be90edf9fe7a8e5e5f031e45442c4
Roderick Hall <rjhall@RodericudentMBP.attlocal.net>
2022-01-26 16:15:46 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const Friends =['Doug','Matt','Cole','Ken','Damon'];
+for( let i = 0; i < Friends.length; i++){
+    console.log(Friends[i])
+
+
+for (let j = 99; j >= 0;j-- ){
+    console.log(Friends[i],j)
+    if (j == 2) {
+        console.log( j+ " lines of code in a file" + j + " Lines of code!" + Friends[i]+ " strikes one out, clears it all out " + j + " lines of code in the file" )
+    }
+     else if (j ===1) {
+        console.log( j+ " line of code in a file" + j + " Line of code!" + Friends[i]+ " strikes one out, clears it all out " + j + " line of code in the file")
+
+      } else {
+        console.log( j+ " lines of code in a file" + j + " Lines of code!" + Friends[i]+ " strikes one out, clears it all out " + j + " lines of code in the file")
+
+   
+    
+}}}
