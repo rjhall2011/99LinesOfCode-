484c75d320957f28ee66830fcf2239d39e9640f1
Roderick Hall <rjhall2018asu@gmail.com>
2022-02-10 15:18:11 -0600

    update

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const Friends =['Doug','Matt','Cole','Ken','Damon'];
-for( let i = 0; i < Friends.length; i++){
-    console.log(Friends[i])
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.getElementById("button");
+
+    button.addEventListener("click", function () {
+        let friends = ['Doug', 'Matt', 'Cole', 'Ken', 'Damon'];
+
+        for (let i = 0; i < friends.length; i++) {
+
+            let div = document.createElement("div");
+            let heading = document.createElement("h3");
+
+            document.body.appendChild(div);
+            div.className = "friends";
+            div.innerHTML = "<h3 class= 'name'>" + friends[i].toUpperCase() + "</h3>"
+
+            for (let j = 99; j >= 0; j--) {
+                //console.log(Friends[i],j)
+                if (j == 2) {
+                    let p = document.createElement("p")
+                    p.innerHTML = j + " lines of code in a file" + j + " Lines of code!" + friends[i] + " strikes one out, clears it all out " + (j - 1) + " lines of code in the file";
+                    div.appendChild(p);
+                }
+                else if (j === 1) {
+                    let p = document.createElement("p")
+                    p.innerHTML = j + " line of code in a file" + j + " Line of code!" + friends[i] + " strikes one out, clears it all out " + (j - 1) + " line of code in the file";
+                    div.appendChild(p);
+                } else {
+                    let p = document.createElement("p")
+                    p.innerHTML = j + " lines of code in a file" + j + " Lines of code!" + friends[i] + " strikes one out, clears it all out " + (j - 1) + " lines of code in the file";
+                    div.appendChild(p);
+                };
+                //console.log(Friends[i])
+
+            };
+
+        }
 
 
-for (let j = 99; j >= 0;j-- ){
-    console.log(Friends[i],j)
-    if (j == 2) {
-        console.log( j+ " lines of code in a file" + j + " Lines of code!" + Friends[i]+ " strikes one out, clears it all out " + j + " lines of code in the file" )
-    }
-     else if (j ===1) {
-        console.log( j+ " line of code in a file" + j + " Line of code!" + Friends[i]+ " strikes one out, clears it all out " + j + " line of code in the file")
-
-      } else {
-        console.log( j+ " lines of code in a file" + j + " Lines of code!" + Friends[i]+ " strikes one out, clears it all out " + j + " lines of code in the file")
-
-   
-    
-}}}
+    })
+})
